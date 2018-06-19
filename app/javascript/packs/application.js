efd77634453b09f0cfed9fac5063a289ea962abb
Arthur Littmann <arthur.littmann@gmail.com>
2018-06-19 11:29:16 +0200

    added owl carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import SweetScroll from 'sweet-scroll';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
 document.addEventListener('DOMContentLoaded', () => {
   const scroller = new SweetScroll({/* some options */});
