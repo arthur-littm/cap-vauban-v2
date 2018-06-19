2ab47442b36fd5b1a057d99a91dba0097d432811
Arthur Littmann <arthur.littmann@gmail.com>
2018-06-19 11:04:14 +0200

    sweet scroll and favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+import SweetScroll from 'sweet-scroll';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scroller = new SweetScroll({/* some options */});
+}, false);
