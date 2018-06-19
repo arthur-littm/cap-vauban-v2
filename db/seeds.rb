d073d86cdaa4bb657210e95fe42a01633636b76f
Arthur Littmann <arthur.littmann@gmail.com>
2018-06-19 15:08:33 +0200

    seeds"

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@ Flat.destroy_all
 
 Flat.create!(
   name: 'Le Port',
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus fugit distinctio nemo quasi, esse dolorem eum error itaque veniam quibusdam eius, illo blanditiis quos minus, culpa, non quod nihil reprehenderit.",
   address: '11 Avenue Saint-Roch, 06600, Antibes',
   kitchen: true,
+  tv: true,
   wifi: true,
   washing_machine: true,
   aircon: true,
@@ -12,6 +14,7 @@ Flat.create!(
 
 Flat.create!(
   name: 'Le Plage',
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus fugit distinctio nemo quasi, esse dolorem eum error itaque veniam quibusdam eius, illo blanditiis quos minus, culpa, non quod nihil reprehenderit.",
   address: '11 Avenue Saint-Roch, 06600, Antibes',
   kitchen: true,
   wifi: true,
