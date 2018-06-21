85011fdbfe3a59aab13b5ede73d3fd233025aee2
Arthur Littmann <arthur.littmann@gmail.com>
2018-06-21 16:49:18 +0100

    added i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { registrations: "registrations"}
-  root to: 'pages#home'
+    scope '(:locale)', locale: /fr|en/ do
+      root to: 'pages#home'
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
